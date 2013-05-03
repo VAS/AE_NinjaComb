49744168e518db06038034e154fd86dc8ad647ad
VAS <studio@vas.it>
2013-05-03 16:49:28 +0300

    Update ninjacomb.jsx

--- ninjacomb.jsx
+++ ninjacomb.jsx
@@ -15,16 +15,16 @@
  * USAGE:
  *    - Order the desired clips by NAME in ASCENDING order, in the project pane.
  *    - Select all of them
- * 	  - Launch the script from the File->Script menu.
+ *    - Launch the script from the File->Script menu.
  *
  * CAVEAT: 
- * 	  - Make sure to select only VIDEO files, no images, no, solids, no folders..
+ *    - Make sure to select only VIDEO files, no images, no, solids, no folders..
  *    - By now the scripts expects the files names to be in the format scene#.shot#.take#.part#.mov
  *    - The files must have unique names
  *    - The original frame rate of the footage must be 24p, encapsulated in 30i by 3:2 pulldown 			
  *
  * TODO:
- * 	  - The script itself should order the clips by name
+ *    - The script itself should order the clips by name
  *    - Making a GUI to let the user specify some parameters like the resize factor, the naming convention or the output file format.
  *    - In case the video has been recorded in 25p the script shouldn't attempt to remove pulldown and perform a simple de-interlace instead.
  *    - Adding support fort 3rd party plugins. E.g. Magic Bullet for the resizing.
