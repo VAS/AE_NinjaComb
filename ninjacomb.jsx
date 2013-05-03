e5d568ad4f96c7f4479d74f4c3f29fb61d1cc477
VAS <studio@vas.it>
2013-05-03 16:52:09 +0300

    Update ninjacomb.jsx

--- ninjacomb.jsx
+++ ninjacomb.jsx
@@ -46,53 +46,59 @@ var curLayer;
 app.beginUndoGroup("VAS_NinjaComb")
 
 //Remove pulldown
-for (i = 0; i < selected.length; i++)
-{
+for (i = 0; i < selected.length; i++) {
     item = selected[i];
     item.mainSource.fieldSeparationType = FieldSeparationType.UPPER_FIELD_FIRST;
     item.mainSource.guessPulldown(PulldownMethod.PULLDOWN_3_2);
 }
 
-for (i = 0; i < selected.length; i++)
-{
+for (i = 0; i < selected.length; i++) {
     item = selected[i];
-    baseName = item.name.split('.'); //Create an array with this structure: [scene, shot, take, part, extension]
+
+    //Create an array with this structure: [scene, shot, take, part, extension]
+    baseName = item.name.split('.');
     
     //Create the new compositions
-    if(contains(blackList, item) == false) //Check if the clip has not already been added to other comps.
-    {
-		compName = baseName[0] + '.' + baseName[1] + '.' + baseName[2] //name of composition: deleting the extension and the part number.
-		curComp = app.project.items.addComp(compName, item.width, item.height, item.pixelAspect, item.duration, item.frameRate); //Create the new comp
+
+    //Check if the clip has not already been added to other comps.
+    if(contains(blackList, item) == false) {
+
+        //name of composition: deleting the extension and the part number.
+		compName = baseName[0] + '.' + baseName[1] + '.' + baseName[2];
+
+        //Create the new comp
+		curComp = app.project.items.addComp(compName, item.width, item.height, item.pixelAspect, item.duration, item.frameRate);
 		curLayer = curComp.layers.add(item);
 		curLayer.scale.setValue([120, 120]);
-		app.project.renderQueue.items.add(curComp); //Add the new comp to the render queue
+
+        //Add the new comp to the render queue
+		app.project.renderQueue.items.add(curComp); 
     }
     
-	//Loop the remaining clips, searching for the other parts of the same takel (start a new counter from where the last stopped).
-    for (var j = (i+1); j < selected.length; j++)
-    {
+	// Loop the remaining clips, searching for the other parts 
+    // of the same takel (start a new counter from where the last stopped).
+    for (var j = (i+1); j < selected.length; j++) {
         var confItem = selected[j];
         var confName = confItem.name.split('.');
         
-		if(isSameTake(baseName, confName) ) //Check if I found another part of the same take.
-        {
+        //Check if I found another part of the same take.
+		if(isSameTake(baseName, confName) ) {
 			curLayer = curComp.layers.add(confItem);//Add the clip to the composition.
 			curLayer.scale.setValue([120, 120]);
 			curLayer.startTime= curComp.duration; //Shift the new clip at the end of the comp.
-             curComp.duration += confItem.duration; 
-             blackList.push(confItem); //Add the clip to blacklist: it's already in it's comp, I don't have to check it again.
+            curComp.duration += confItem.duration; 
+
+            //Add the clip to blacklist: it's already in it's comp, I don't have to check it again.
+            blackList.push(confItem);
         }
     }
 }
 
 app.endUndoGroup() 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 //Function to check if two clips are part of the same take. Returns true if 'scene', 'shot' and 'take' fields of the two file names are the same. 
-function isSameTake(a, b)
-{
+function isSameTake(a, b) {
     if ( a[0]  == b[0] && a[1]  == b[1] && a[2]  == b[2])
         return true;
     else
